ca2be1f3601fe0addc679b0322390a0c4b822e87
leviethung220703@gmail.com <leviethung220703@gmail.com>
2024-06-04 03:31:01 +0200

    [Add]_Payment method DAO and Repository_FE

--- BadmintonCenter/BadmintonCenter.DataAcess/DAO/UserPackageDAO.cs
+++ BadmintonCenter/BadmintonCenter.DataAcess/DAO/UserPackageDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BadmintonCenter.BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,57 @@ using System.Threading.Tasks;
 
 namespace BadmintonCenter.DataAcess.DAO
 {
-    public class UserPackageDAO
+    public interface IUserPackageDAO
     {
+        Task<UserPackage> GetUserPackageByIdAsync(int userPackageId);
+        Task<UserPackage> GetUserPackageByUserIdAsync(int userId);
+        Task<List<UserPackage>> GetAllUserPackageAsync();
+        Task AddUserPackageAsync(UserPackage userPackage);
+        Task UpdateUserPackageAsync(UserPackage userPackage);
+        Task DeleteUserPackageAsync(int userPackageId);
+    }
 
+    public class UserPackageDAO : IUserPackageDAO
+    {
+        private readonly BadmintonDbContext _context;
+
+        public UserPackageDAO(BadmintonDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUserPackageAsync(UserPackage userPackage)
+        {
+            await _context.UserPackages.AddAsync(userPackage);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserPackageAsync(int packageId)
+        {
+            var userPackage = await _context.UserPackages.FirstOrDefaultAsync(p => p.PackageId == packageId);
+            _context.UserPackages.Remove(userPackage);
+        }
+
+        public async Task<List<UserPackage>> GetAllUserPackageAsync()
+        {
+            return await _context.UserPackages.ToListAsync();
+        }
+
+        public async Task<UserPackage> GetUserPackageByIdAsync(int packageId)
+        {
+            var userPackage = await _context.UserPackages.FirstOrDefaultAsync(a => a.PackageId == packageId);
+            return userPackage;
+        }
+
+        public Task<UserPackage> GetUserPackageByUserIdAsync(int userId)
+        {
+            return _context.UserPackages.FirstOrDefaultAsync(a => a.UserId == userId);
+        }
+
+        public async Task UpdateUserPackageAsync(UserPackage userPackage)
+        {
+            _context.UserPackages.Update(userPackage);
+            await _context.SaveChangesAsync();
+        }
     }
 }
