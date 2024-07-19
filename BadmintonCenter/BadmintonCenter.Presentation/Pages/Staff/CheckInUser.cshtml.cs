675434d21cb827810b79904870396c1d3349b681
Xion Sir <trongle278@gmail.com>
2024-07-19 21:46:24 +0700

    Add checkcourt

---
+++ BadmintonCenter/BadmintonCenter.Presentation/Pages/Staff/CheckInUser.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BadmintonCenter.BusinessObject.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using BadmintonCenter.Common.Enum.Status;
+using Microsoft.EntityFrameworkCore;
+
+namespace BadmintonCenter.Presentation.Pages.Staff
+{
+    public class CheckInUserModel : PageModel
+    {
+        
+    }
+}
